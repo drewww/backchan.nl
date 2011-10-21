eaeb498ec2f2ac8dd7d90ecaa99abb81d5bf25b2
Drew Harry <drew.harry@gmail.com>
2011-10-21 13:30:19 -0400

    Switching over to compass/sass for stylesheets. Not using any sass features yet, just have the mechanics setup for it. Using a config.rb file from cfd.

---
+++ static/css/sass/config.rb
@@ -0,0 +1,12 @@
+project_path = "."
+css_dir      = ".."
+sass_dir     = "."
+
+output_style = :expanded
+line_numbers = false
+line_comments = false
+load_paths = false
+
+# maybe vestigal, according to cfd
+images_dir   = "/img/"
+http_path    = "/css/"
