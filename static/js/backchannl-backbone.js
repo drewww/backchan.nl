336909252dd26b9c0d42e8b26585b5db3ecb9dad
Drew Harry <drew.harry@gmail.com>
2011-10-22 19:14:32 -0400

    Wrote a collection + collection view stub + some code to take advantage of it. It sort of, kind of works?

--- static/js/backchannl-backbone.js
+++ static/js/backchannl-backbone.js
@@ -62,3 +62,44 @@ var PostView = Backbone.View.extend({
     }
 });
 
+var NewPostList = Backbone.Collection.extend({
+    
+    model: Post,
+    
+    first: function() {
+        return this.at(0);
+    },
+    
+    comparator: function(post) {
+        return post.get("timestamp");
+    }
+});
+
+
+var NewPostListView = Backbone.View.extend({
+    tagName: 'div',
+    
+    template: _.template('<div id="new-post-container"></div>'),
+    
+    events: {},
+    
+    initialize: function() {
+        this.model.bind('change', this.render, this);
+        this.model.bind('add', this.render, this);
+        this.model.bind('remove', this.render, this);
+    },
+    
+    render: function() {
+        console.log("rendering post list view");
+        // get the current first item from the collection and display it.
+        $(this.el).children().remove();
+        
+        var currentShownPost = this.model.first();
+        
+        $(this.el).append(new PostView({model:currentShownPost}).render().el);
+        
+        return this;
+    }
+    
+});
+
