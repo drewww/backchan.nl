db9c3b94f9df178ed7b7a4144ea7daf14ff00f43
Drew Harry <drew.harry@gmail.com>
2011-10-22 21:47:38 -0400

    Got empying working, with appropriate notices for empty list views.

--- static/js/backchannl-backbone.js
+++ static/js/backchannl-backbone.js
@@ -2,36 +2,39 @@ var Post = Backbone.Model.extend({
     initialize: function() {
         console.log("initing post");
     },
-    
+
     add_vote: function(at_timestamp) {
-        if(_.isUndefined(at_timestamp) || _.isNull(at_timestamp)) {
+        if (_.isUndefined(at_timestamp) || _.isNull(at_timestamp)) {
             at_timestamp = Date.now();
         }
-        
+
         var currentVoteList = this.get("votes");
         currentVoteList.push(at_timestamp);
-        this.set({"votes":currentVoteList});
-        
+        this.set({
+            "votes": currentVoteList
+        });
+
         console.log("Adding vote, total: " + this.get("votes").length);
     },
-    
-    
+
+
     defaults: function() {
         return {
-        from_name: "default name",
-        from_affiliation: "nowhere",
-        text: "default text",
-        timestamp: Date.now(),
-        votes: [Date.now()]};
+            from_name: "default name",
+            from_affiliation: "nowhere",
+            text: "default text",
+            timestamp: Date.now(),
+            votes: [Date.now()]
+        };
     }
-    
+
 });
 
 
 var PostView = Backbone.View.extend({
-    
+
     tagName: 'div',
-    
+
     template: _.template('<div class="post">\
     <div class="button-container">\
     <div class="vote-button">\
@@ -54,25 +57,26 @@ var PostView = Backbone.View.extend({
     </div>\
     <br class="clear">\
     </div>'),
-    
+
     events: {
-        "click .dismiss-button" : "dismiss",
-        "click .vote-button" : "vote"},
-    
+        "click .dismiss-button": "dismiss",
+        "click .vote-button": "vote"
+    },
+
     initialize: function() {
         this.model.bind('change', this.render, this);
         this.model.bind('destroy', this.remove, this);
     },
-    
+
     render: function() {
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
     },
-    
+
     dismiss: function() {
         console.log("dismiss");
     },
-    
+
     vote: function() {
         this.model.add_vote();
         this.render();
@@ -80,13 +84,13 @@ var PostView = Backbone.View.extend({
 });
 
 var PostList = Backbone.Collection.extend({
-    
+
     model: Post,
-    
+
     first: function() {
         return this.at(0);
     },
-    
+
     comparator: function(post) {
         return post.get("timestamp");
     }
@@ -96,62 +100,72 @@ var PostList = Backbone.Collection.extend({
 var NewPostListView = Backbone.View.extend({
     tagName: 'div',
     id: 'new',
-    
+
     template: _.template('<h1>new</h1>'),
 
     // We may not need any here, not sure yet.
     events: {},
-    
+
     initialize: function() {
-        this.collection.bind('change', this.render, this);
         this.collection.bind('add', this.render, this);
         this.collection.bind('remove', this.render, this);
     },
-    
+
     render: function() {
         console.log("rendering post list view");
         // get the current first item from the collection and display it.
-        
         $(this.el).html(this.template());
-        
-        var currentShownPost = this.collection.first();
-        $(this.el).append(new PostView({model:currentShownPost}).render().el);
-        
+
+        var currentDisplayedPost = this.collection.first();
+        if (!_.isUndefined(currentDisplayedPost)) {
+            $(this.el).append(new PostView({
+                model: currentDisplayedPost
+            }).render().el);
+        } else {
+            $(this.el).append("<div class='empty-notice'>\
+            there are no new posts right now</div>");
+        }
+
         return this;
     }
-    
+
 });
 
 
 // If I was smart I'd be extending these from some base class, but let me get
 // it all working first and then take it from there.
 var HotPostListView = Backbone.View.extend({
-   tagName: 'div',
-   id: 'hot',
-   
-   template: _.template('<h1>hot</h1>'),
-   
-   events: {},
-   
-   initialize: function() {
-       this.collection.bind('change', this.render, this);
-       this.collection.bind('add', this.render, this);
-       this.collection.bind('remove', this.render, this);
-   },
-   
-   render: function() {
-       console.log("rendering HOT post list view");
-       // get the current first item from the collection and display it.
-       
-       $(this.el).html(this.template());
-       
-       var currentShownPost = this.collection.first();
-       
-       if(!_.isUndefined(currentShownPost))
-        $(this.el).append(new PostView({model:currentShownPost}).render().el);
-       
-       return this;
-   }
-   
+    tagName: 'div',
+    id: 'hot',
+
+    template: _.template('<h1>hot</h1>'),
+
+    events: {},
+
+    initialize: function() {
+        this.collection.bind('add', this.render, this);
+        this.collection.bind('remove', this.render, this);
+    },
+
+    render: function() {
+        console.log("rendering HOT post list view");
+        // get the current first item from the collection and display it.
+        $(this.el).html(this.template());
+
+        var currentDisplayedPost = this.collection.first();
+
+        if (!_.isUndefined(currentDisplayedPost)) {
+            $(this.el).append(new PostView({
+                model: currentDisplayedPost
+            }).render().el);
+        } else {
+            $(this.el).append("<div class='empty-notice'>\
+            there are no hot posts right now</div>");
+        }
+
+
+        return this;
+    }
+
 });
 
