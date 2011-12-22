92d2627f709ce0ca124bcee114abaf3648cdddcd
Drew Harry <drew.harry@gmail.com>
2011-12-22 17:40:59 -0500

    A few more tests for post. I feel like coverage is basically complete now.

--- test/client-model.js
+++ test/client-model.js
@@ -14,6 +14,21 @@ describe('models: ', function() {
                 newPost.votes().should.equal(0);
             });
             
+        it('should respond to alterate constructor values properly', 
+            function() {
+            var newPost = new model.Post({"fromName":"Drew",
+                "fromAffiliation":"MIT Media Lab",
+                "fromId":0,
+                "text":"Welcome to backchan.nl!"});
+                
+                should.exist(newPost);
+                newPost.get("fromName").should.equal("Drew");
+                newPost.get("fromAffiliation").should.equal("MIT Media Lab");
+                newPost.get("fromId").should.equal(0);
+                newPost.get("text").should.equal("Welcome to backchan.nl!");
+                newPost.votes().should.equal(0);
+        });
+        
         it('should accumulate votes properly', function() {
             
             var newPost = new model.Post();
@@ -46,6 +61,19 @@ describe('models: ', function() {
             newPost.hasVoteFrom(0).should.be.true;
             newPost.hasVoteFrom(1).should.be.false;
             newPost.hasVoteFrom(2).should.be.false;
+        });
+        
+        it('should return recentVotes within a specified time window',
+            function() {
+                var newPost = new model.Post();
+                should.exist(newPost);
+                
+                newPost.addVote(Date.now()-5000, 1);
+                newPost.addVote(Date.now()-20000, 2);
+
+                
+                newPost.recentVotes(10000).should.equal(1);
+                newPost.mostRecentVote().should.have.property('id', 1);
         });
     });    
 });
