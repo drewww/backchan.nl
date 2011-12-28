b67703cfc63fad7ef33e5ff9154c10294335acc0
Drew Harry <drew.harry@gmail.com>
2011-12-28 16:41:25 -0500

    Refactored how things work in the client to use Backbone events to publish state changes. Makes testing a breeze now - no more passing around callback objects everywhere.

---
+++ test/test.client.js
@@ -0,0 +1,35 @@
+var should = require('should'),
+    server = require('../lib/server.js'),
+    client = require('../static/js/client.js');
+   
+
+describe('client-server communication', function(){
+    describe('client', function(done){
+        beforeEach(function(done) {
+            server.start("localhost", 8181, done);
+        });
+        afterEach(function(done) {
+            server.stop(done);
+        });
+        
+        it('should connect properly', function(done){
+                // Once the server has started, make a client.
+                var cm = new client.ConnectionManager();
+                
+                cm.bind("state.CONNECTED", function() {
+                    done();
+                });
+                cm.connect("localhost", 8888);
+        });
+        
+        it('should handle identify commands', function(done) {
+            var cm = new client.ConnectionManager();
+            
+            cm.bind("state.CONNECTED", function() {
+                cm.identify("Test User", "Test Affiliation");
+                done();
+            });
+            cm.connect("localhost", 8888);
+        });
+    });
+});
