b3379cba32408f6cdd3b8c184207b71d5147003e
Drew Harry <drew.harry@gmail.com>
2012-01-13 17:44:55 -0500

    Got journaling to start up and provisionally sort of work on the write side.

---
+++ test/test.journaling.js
@@ -0,0 +1,42 @@
+var should = require('should'),
+    server = require('../lib/server.js'),
+    client = require('../static/js/client.js'),
+    model = require('../lib/server-model.js');
+   
+
+var curServer, curClient;
+
+
+describe('journaling', function() {
+    before(function(done) {
+        curServer = new server.BackchannlServer();
+        curServer.bind("started", done);
+        curServer.start("localhost", 8181);
+    });
+    
+    beforeEach(function(done) {
+        curServer.reset({"test-event":true, "journaling":true});
+
+        curClient = new client.ConnectionManager();
+        
+        curClient.bind("state.JOINED", function() {
+            done();
+        });
+        
+        curClient.connect("localhost", 8181, {
+            "auto-identify":true,
+            "auto-join":true
+        });
+    });
+    
+    after(function(done) {
+        curServer.bind("stopped", done);
+        curServer.stop();
+    });
+    
+    it('should not crash when journaling is turned on', function(){
+      
+      curServer.events.get(0).get("journaler").should.exist;
+      
+    });
+});
