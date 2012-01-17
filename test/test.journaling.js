969527c9010b9e90a5c82fb4c053a742022fa95f
Drew Harry <drew.harry@gmail.com>
2012-01-17 16:22:49 -0500

    Got simple journaling working for chat + post + vote, plus a test that checks that the files are legit.

--- test/test.journaling.js
+++ test/test.journaling.js
@@ -1,6 +1,8 @@
 var should = require('should'),
     server = require('../lib/server.js'),
     client = require('../static/js/client.js'),
+    fs = require('fs'),
+    actions = require('../lib/actions.js'),
     model = require('../lib/server-model.js');
    
 
@@ -15,6 +17,9 @@ describe('journaling', function() {
     });
     
     beforeEach(function(done) {
+        // delete the contents of the event directory. 
+        actions.flushAllJournals();
+        
         curServer.reset({"test-event":true, "journaling":true});
 
         curClient = new client.ConnectionManager();
@@ -35,6 +40,46 @@ describe('journaling', function() {
     });
     
     it('should not crash when journaling is turned on', function(){
+        
+    });
+    
+    it('should create a log file for the event when actions happen', function(done){
+
+        curClient.bind("message.chat", function() {
+            curClient.post("FIRST");
+        });
+        
+        curClient.bind("message.post", function() {
+            // now check the log file to see if we've got all the data.
+            var filename = actions.baseEventsDir + curClient.event.id + ".act";
+            var file = fs.readFileSync(filename, 'utf8').split("\n");
+            
+            file.should.exist;
+            
+            // it's two lines long, but the last line ends in a \n so it's 
+            //technically 3 total lines.
+            file.length.should.equal(2+1);
+            
+            var chatAction = JSON.parse(file[0]);
+            
+            chatAction.type.should.equal("CHAT");
+            chatAction.params.text.should.equal("hello world!");
+            chatAction.eventId.should.equal(0);
+            chatAction.userId.should.equal(0);
+            
+            
+            var postAction = JSON.parse(file[1]);
+            
+            postAction.type.should.equal("POST");
+            postAction.params.text.should.equal("FIRST");
+            postAction.eventId.should.equal(0);
+            postAction.userId.should.equal(0);
+            
+            done();
+        });
+        
+        
+        curClient.chat("hello world!");
         
     });
 });
