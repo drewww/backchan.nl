523294cd0f365a47653640f1e1222ef9e53c0990
Drew Harry <drew.harry@gmail.com>
2012-01-24 12:18:05 -0500

    Made some minor changes to how the server is started to take advantage of all the internal features that exist. Made a test page to try to get mocha testing working on.

--- backchannl.js
+++ backchannl.js
@@ -1,6 +1,7 @@
 var program = require('commander'),
     logger = require('winston'),
-    server = require('./lib/server.js');
+    server = require('./lib/server.js'),
+    dispatch = require('./lib/dispatch.js');
 
 logger.cli();
 logger.default.transports.console.timestamp = true;
@@ -9,6 +10,9 @@ logger.default.transports.console.timestamp = true;
 
 program.version('0.1')
     .option('-p, --port [num]', 'Set the server port (default 8080)')
+    .option('-P, --production', 'Sets the server to production mode (default false)')
+    .option('-l, --load', 'Loads users and events from disk, otherwise starts fresh (default false)')
+    .option('-d, --dispatcher [name]', 'Sets the dispatcher type (default \"base\")')
     .parse(process.argv);
 
 var host = "localhost";
@@ -19,11 +23,47 @@ if(program.args.length==1) {
 } else {
     logger.info("Too many command line arguments. Expected 0 or 1.")
 }
+
 var port = 8080;
+var production = false;
+var load = false;
+
 if(program.port) {
     logger.info("Setting port to " + program.port);
     port = program.port;
 }
 
-var theServer = new server.BackchannlServer();
+if(program.production) {
+    production = true;
+    load = true;
+} else {
+    // if we're not in production mode, then check and see if we want to
+    // load. if we don't load, the server auto-creates a dummy event for
+    // us to test with.
+    
+    if(program.load) {
+        load = true;
+    }
+}
+
+var dispatch = "base";
+if(program.dispatch) {
+    if(dispatch.getDispatcherForName(program.dispatch) == null) {
+        logger.warning(program.dispatch + " is an invalid dispatch type. Defaulting to 'base'.");
+    } else {
+        dispatch = program.dispach;
+    }
+}
+
+logger.info("production? " + production);
+logger.info("load? " + load);
+logger.info("dispatch: " + dispatch);
+
+var theServer = new server.BackchannlServer({
+    "production":production,
+    "load":load,
+    "dispatcher":dispatch});
+    
+    
+logger.info("starting server on " + host + ":" + port);
 theServer.start(host, port);
