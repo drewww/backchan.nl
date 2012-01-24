faf0b3f691a7c708a69bd039a76fdc87939c48a9
Drew Harry <drew.harry@gmail.com>
2012-01-24 16:23:26 -0500

    Stubs for all the major views we'll need, just to get a sense of what needs to be done.

--- static/js/views.js
+++ static/js/views.js
@@ -4,6 +4,27 @@
 // quite yet. So we're not going to do the on-server-or-not dance we do
 // in client.js and model.js, since this is purely local. We assume that
 // we have model and already loaded here.
+
+// Here's the hierarchy:
+// BackchannlBar    contains everything else; absolutely positioned on the 
+//                  edge of the screen.
+//
+// ChatBarView      contains the chat entry text field, and is the parent of 
+//                  two ChatListViews, one with transparent background + no
+//                  scroll, one with an opaque background + scroll
+//
+// ChatMessageView  each individual chat message. Will also have instances of
+//                  UserView within it.
+//
+// PostListView     flush with the left edge of the screen, contains the 
+//                  posts header, a text field for entering new posts, and
+//                  the scrollable list of current posts
+//
+// PostView         The view for each individual post. Has lots of features +
+//                  events that we won't go into here. Has UserViews in it, 
+//                  but that's about it. 
+
+
 views = {};
 
 views.PostView = Backbone.View.extend({
@@ -24,4 +45,28 @@ views.PostView = Backbone.View.extend({
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
     },
+});
+
+views.PostListView = Backbone.View.extend({
+    
+});
+
+views.UserView = Backbone.View.extend({
+    
+});
+
+views.ChatMessageView = Backbone.View.extend({
+    
+});
+
+views.ChatListView = Backbone.View.extend({
+    
+});
+
+views.ChatBarView = Backbone.View.extend({
+    
+});
+
+views.BackchannlBar = Backbone.View.extend({
+    
 });
