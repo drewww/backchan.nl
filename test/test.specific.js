f0445d8080aaaa266c08ab9827f746d7c6c0fa5c
Drew Harry <drew.harry@gmail.com>
2012-02-21 18:15:40 -0500

    Added a simple test framework file for developing new tests. Try not to check this in, but it's a useful place to develop new tests and make sure a specific test is working without running the whole suite.>

---
+++ test/test.specific.js
@@ -0,0 +1,32 @@
+var should = require('should'),
+    server = require('../lib/server.js'),
+    client = require('../static/js/client.js'),
+    model = require('../lib/server-model.js');
+   
+
+var curServer, curClient;
+var clients;
+
+describe('join/leave process', function() {
+    before(function(done) {
+        curServer = new server.BackchannlServer();
+        curServer.bind("started", done);
+        curServer.start("localhost", 8181);
+    });
+    beforeEach(function(done) {
+        curServer.reset({"test-event":true});
+
+        curClient = new client.ConnectionManager();
+        curClient.bind("state.CONNECTED", done);
+        curClient.connect("localhost", 8181);
+        
+    });
+    after(function(done) {
+        curServer.bind("stopped", done);
+        curServer.stop();
+    });
+
+    it('test event', function(done){
+        done();
+    });
+});
