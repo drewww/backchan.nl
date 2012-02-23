0083518c8f22a5b25590ceff0c45f821d2dbc13c
Drew Harry <drew.harry@gmail.com>
2012-02-23 17:04:43 -0500

    Fixed the crashing issue in the tests, not totally sure how/why.

--- test/test.client.js
+++ test/test.client.js
@@ -993,4 +993,75 @@ describe('client-server communication', function(){
             });
         });
     });
+    
+    
+    describe('join/leave process', function() {
+        before(function(done) {
+            curServer = new server.BackchannlServer();
+            curServer.bind("started", done);
+            curServer.start("localhost", 8181);
+        });
+        beforeEach(function(done) {
+            curServer.reset({"test-event":true, "dispatcher":"spread"});
+
+            curClient = new client.ConnectionManager();
+            curClient.bind("state.CONNECTED", done);
+            curClient.connect("localhost", 8181);
+
+        });
+        after(function(done) {
+            curServer.bind("stopped", done);
+            curServer.stop();
+        });
+
+        it('user leaving an event they are multiply joined to wont cause all sockets to leave', function(done){
+            curClient.bind("state.IDENTIFIED", function() {
+                curClient.join(0);
+            });
+
+            curClient.bind("state.JOINED", function() {
+                secondClient.connect("localhost", 8181);
+            });
+
+            curClient.identify("Test", "Organization");
+
+
+            var secondClient = new client.ConnectionManager();
+            var thirdClient = new client.ConnectionManager();
+
+            secondClient.bind("state.CONNECTED", function() {
+                secondClient.identify("Test", "Organization");
+            });
+
+            thirdClient.bind("state.CONNECTED", function() {
+                thirdClient.identify("Another Person", "Organization");
+            })
+
+            secondClient.bind("state.IDENTIFIED", function() {
+                secondClient.join(0);
+            });
+
+            thirdClient.bind("state.IDENTIFIED", function() {
+                thirdClient.join(0);
+            });
+
+            thirdClient.bind("state.JOINED", function() {
+                thirdClient.post("post after leaving");
+            });
+
+            secondClient.bind("state.JOINED", function() {
+                secondClient.leave();
+            });
+
+            secondClient.bind("message.leave-ok", function() {
+                thirdClient.connect("localhost", 8181);
+            });
+
+            curClient.bind("message.post", function(post) {
+                post.should.exist
+                post.get("text").should.equal("post after leaving");
+                done();
+            });
+        });
+    });
 });
--- test/test.specific.js
+++ test/test.specific.js
@@ -26,53 +26,5 @@ describe('join/leave process', function() {
         curServer.stop();
     });
 
-    it('user leaving an event they are multiply joined to wont cause all sockets to leave', function(done){
-        curClient.bind("state.IDENTIFIED", function() {
-            curClient.join(0);
-        });
-        
-        curClient.bind("state.JOINED", function() {
-            secondClient.connect("localhost", 8181);
-        });
-        
-        curClient.identify("Test", "Organization");
-    
-    
-        var secondClient = new client.ConnectionManager();
-        var thirdClient = new client.ConnectionManager();
-    
-        secondClient.bind("state.CONNECTED", function() {
-            secondClient.identify("Test", "Organization");
-        });
-        
-        thirdClient.bind("state.CONNECTED", function() {
-            thirdClient.identify("Another Person", "Organization");
-        })
-    
-        secondClient.bind("state.IDENTIFIED", function() {
-            secondClient.join(0);
-        });
-        
-        thirdClient.bind("state.IDENTIFIED", function() {
-            thirdClient.join(0);
-        });
-        
-        thirdClient.bind("state.JOINED", function() {
-            thirdClient.post("post after leaving");
-        });
-    
-        secondClient.bind("state.JOINED", function() {
-            secondClient.leave();
-        });
-        
-        secondClient.bind("message.leave-ok", function() {
-            thirdClient.connect("localhost", 8181);
-        });
-    
-        curClient.bind("message.post", function(post) {
-            post.should.exist
-            post.get("text").should.equal("post after leaving");
-            done();
-        });
-    });
+    it('should be a placeholder');
 });
